5a64ff4d0de57cd8a797b7c81df9e493c50af42f
Eugene Molari <eugelogic@gmail.com>
2021-11-21 10:19:03 +0000

    Add Header component

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import styles from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.siteTitle}>
+                <Link href='/'>
+                    <a>Eugene Molari Photography</a>
+                </Link>
+            </div>
+            <div className={styles.siteDescription}>
+                <p>Carpe Lucem</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,12 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={styles.header}>
-        <Link href='/'>
-          <a><h1>Eugene Molari Photography</h1></a>
-        </Link>
-        <p>Carpe Lucem</p>
-      </header>
+      <Header />
 
       <main>
 
