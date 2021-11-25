8787a305476569e9461249de13e65ad796c4aa87
Eugene Molari <eugelogic@gmail.com>
2021-11-25 21:06:02 +0000

    Introduce a Gallery folder

--- pages/gallery/leverton-st-kentish-town-london-may-2018.js
+++ pages/gallery/leverton-st-kentish-town-london-may-2018.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Layout from '../components/Layout'
-import styles from '../styles/GridAlt.module.css'
+import Layout from '../../components/Layout'
+import styles from '../../styles/GridAlt.module.css'
 
 const LevertonSt = () => {
 
