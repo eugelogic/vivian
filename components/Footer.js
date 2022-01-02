ef74304528770818bcb9fa42cf4847f079ea037e
Eugene Molari <eugelogic@gmail.com>
2022-01-02 11:13:51 +0000

    Edit copyright message in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.scss'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>Any reproduction of these images is strickly prohibited without the express permission of the author.</p>
+            <p>Any reproduction of these images or videos is strickly prohibited without the express permission of the author.</p>
             <p>&copy; 2006-{new Date().getFullYear()} Eugene Molari Photography</p>
       </footer>
     )
